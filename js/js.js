66fc1e15f560ae77db459d99106dd9a62a4e7b36
stoglestudios <brandongaius@gmail.com>
2015-05-29 16:20:40 -0700

    update search functions to include other dbs (note mech and kids are just counters at this point)

--- js/js.js
+++ js/js.js
@@ -2,14 +2,17 @@
 
 // Search Products Functioon
 function searchProducts(evt) {
+    var numFound = 0;
+    var searchCompletes = 0;
     $results = $("#results");
-    $submit = $("form input:last-child");
-    $submit.text("Searching...");
     evt.preventDefault();
     $results.html( "" );
+    $submit = $("form input:last-child");
+    $submit.prop("value", "Searching...");
     // grab search params, format / Set Vars
     var searchFor = $("input").val();
     var searchInCat = $("select").val();
+    var displayCat;
     $results = $("#results");
     if(!searchInCat) {
         searchInCat = "";
@@ -28,10 +31,53 @@ function searchProducts(evt) {
     var merchandiseDataURL = dataDir + "merchandise.json";
     // Get Graphic Novel Data
     
-    if ( searchInCat === "graphicnovels" || searchInCat === "" ) {
+    // Build Found / No Matches Responses
+    var displayNotFound = '<h1 id="NumOfMatches"></h1>';
+    displayResults += "<p>";
+    
+    if (searchFor != "" && searchFor != "all") {
+        ifFor = ' for <b>' + searchFor + '</b> ';
+    } else {
+        ifFor = " for <b>Anything</b>";
+    }
+    if (searchInCat != "") {
+        if (searchInCat === "childrensbooks") {
+            displayCat = "Children\'s Books";
+        } else if (searchInCat === "graphicnovels") {
+            displayCat = "Graphic Novels";
+        } else if (searchInCat === "merchandise") {
+            displayCat = "Merchandise";
+        } else if ( searchInCat === "all" ) {
+            displayCat = "All Catagories";
+        }
+        ifCat = ' in <b>' + displayCat + '</b>';
+    } else {
+        ifCat = ' in <b>Any</b> Catagory';
+    }
+    startResponce = 'You Searched';
+    displayNotFound += startResponce + ifFor + ifCat;
+    displayNotFound += '<br><br></p>';
+    $results.prepend(displayNotFound);
+    
+    //start timer?
+    /*
+    function checkSearchProgress() {
+        if ( searchCompletes > 0) {
+            $submit.prop("value", "Submit");
+            $("#numOfMatches").text( numFound + " Matches" );
+            clearTimeout( progTimer );
+            alert("Finished searching...");
+        } else {
+            alert("Still searching...");
+        }
+    }
+    var progTimer = setTimeout( function () { checkSearchProgress() }, 200);
+    */
+    if ( searchInCat === "graphicnovels" || searchInCat === "" || searchInCat === "all") {
         $.getJSON( graphicNovelDataURL, function ( response ) {
             var displayHTML = '<ul class="merchList">';
             $.each( response, function ( index, value ) {
+                numFound++;
                 displayHTML += '<li>';
                     if ( value.coverPhoto != "" ) {
                         displayHTML += '<img alt="' + value.series + ': ' + value.name + ', part ' + value.partNum + ' Cover Image" src="' + photoDir + value.coverPhoto + '">';
@@ -62,36 +108,35 @@ function searchProducts(evt) {
             $(".avail").click( function () {
                 alert("This feature is currently unavaible. Sorry for the inconvience.");
             }); // end click()
-            //$submit.text() = "Submit";
+            searchCompletes++;
+            $submit.prop("value", "Submit");
+            $("#NumOfMatches").text( numFound + " Matches" );
         }); // end getJSON
     } //end If GN
     
     // Get Childrens Books Data
-
+    if ( searchInCat === "childrensbooks" || searchInCat === "" || searchInCat === "all" ) {
+        $.getJSON( childrensBooksDataURL, function ( response ) {
+            //var displayHTML = '<ul class="merchList">';
+            $.each( response, function ( index, value ) {
+                numFound++;
+            }); // end each
+            $submit.prop("value", "Submit");
+            $("#NumOfMatches").text( numFound + " Matches" );
+        }); // end getJSON
+    } 
     // Get Merchandise Data
+    if ( searchInCat === "merchandise" || searchInCat === "" || searchInCat === "all" ) {
+        $.getJSON( merchandiseDataURL, function ( response ) {
+            //var displayHTML = '<ul class="merchList">';
+            $.each( response, function ( index, value ) {
+                numFound++;
+            }); // end each
+            $submit.prop("value", "Submit");
+            $("#NumOfMatches").text( numFound + " Matches" );
+        }); // end getJSON
+    } 
     
-    // Build Found / No Matches Responses
-    var displayNotFound = "<h1>No Matches</h1>";
-    displayResults += "<p>";
-    
-    if (searchFor != "") {
-        ifFor = " for <b>" + searchFor + "</b> ";
-    } else {
-        ifFor = " for <b>Anything</b>";
-    }
-    if (searchInCat != "") {
-        ifCat = " in <b>" + searchInCat + "</b>";
-    } else {
-        ifCat = " in <b>Any</b> Catagory";
-    }
-    if ( searchFor != "" || searchInCat != "" ) {
-        startResponce = "You Searched";
-    } else {
-        startResponce = "Try entering a Search Term and/or Selecting a Catagory";
-    }
-    displayNotFound += startResponce + ifFor + ifCat;
-    displayNotFound += "<br><br></p>";
-    $results.prepend(displayNotFound);
 }
 
 $( document ).ready( function () {
