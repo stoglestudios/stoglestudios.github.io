4e4862c64e08d6b93c1505abb7ed01f1428425ad
stoglestudios <brandongaius@gmail.com>
2015-05-29 13:23:54 -0700

    test added functions

--- js/js.js
+++ js/js.js
@@ -23,23 +23,29 @@ function searchProducts(evt) {
     var childrensBooksDataURL = dataDir + "childrensbooks.json";
     var graphicNovelDataURL = dataDir + "graphicnovels.json";
     var merchandiseDataURL = dataDir + "merchandise.json";
+    // Get Graphic Novel Data
     
-    // Get Data
-    $.getJSON( graphicNovelDataURL, function ( response ) {
-		var displayHTML = '<ul class="merchList">';
-		$.each( response, function ( index, value ) {
-			displayHTML += '<li>';
-            displayHTML += '<img src="' + photoDir + value.coverPhoto + '">';
-            displayHTML += '<h1>' + value.name + '</h1>';
-            displayHTML += '<h3>Issue: ' + value.issue + ' - <i>part ' + value.partNum + ' of ' + value.ofPart + '</i></h3>';
-            displayHTML += '<p>' + value.description + '</p>';
-			displayHTML += '</li>';
-		});
-		displayHTML += '</ul>';
-		$results.html( displayHTML );
-	});
+    if ( searchInCat === "graphicnovels" || searchInCat === "" ) {
+        $.getJSON( graphicNovelDataURL, function ( response ) {
+            var displayHTML = '<ul class="merchList">';
+            $.each( response, function ( index, value ) {
+                displayHTML += '<li>';
+                    displayHTML += '<img alt="' + value.coverPhoto + '" src="' + photoDir + value.coverPhoto + '">';
+                    displayHTML += '<button>' + value.price + '</button>';
+                    displayHTML += '<h1>' + value.name + ' <i>(part ' + value.partNum + ' of ' + value.ofPart + ')</i></h1>';
+                    displayHTML += '<h3>' + value.series + ' - Issue: ' + value.issue + '</h3>';
+                    displayHTML += '<p>' + value.description + '</p>';
+                displayHTML += '</li>';
+            });
+            displayHTML += '</ul>';
+            $results.append( displayHTML );
+        });
+    }
+    // Get Childrens Books Data
+
+    // Get Merchandise Data
     
-    // Build No Matches Responses -> Currently Temp Output
+    // Build Found / No Matches Responses
     var displayNotFound = "<h1>No Matches</h1>";
     displayResults += "<p>";
     
@@ -60,7 +66,7 @@ function searchProducts(evt) {
     }
     displayNotFound += startResponce + ifFor + ifCat;
     displayNotFound += "<br><br></p>";
-    $results.html(displayNotFound);
+    $results.prepend(displayNotFound);
 }
 
 $( document ).ready( function () {
