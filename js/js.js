3674144ffec89e0d83079ce6b78aefb5a393302f
Brandon Ogle <brandon@angelvisiontech.com>
2015-06-02 12:13:47 -0700

    added multiple search term functionality and a few misc cleanups

--- js/js.js
+++ js/js.js
@@ -1,46 +1,57 @@
 //Authored by Brandon Gaius Ogle
 
-// Search Products Functioon
+//Global Vars
+
+// Search Products Function - Runs on seacrh button click
+
 function searchProducts(evt) {
-    var numFound = 0;
-    var searchCompletes = 0;
-    $results = $("#results");
-    evt.preventDefault();
-    $results.html( "" );
-    $submit = $("form input:last-child");
-    $submit.prop("value", "Searching...");
-    // grab search params, format / Set Vars
-    var searchFor = $("input").val();
-    searchFor = searchFor.toLowerCase();
-    var searchInCat = $("select").val();
-    var displayCat;
-    $results = $("#results");
-    if(!searchInCat) {
-        searchInCat = "";
-    }
-    if(!searchFor) {
-        searchFor = "";
-    }
-    console.log("Submit Pressed with Params: " + searchFor + " & " + searchInCat);
-    
-    // Set vars/objs
-    var displayResults;
+    // vars
+    var $submit = $("form input:last-child");
+    var $results = $("#results");
     var dataDir = "products/";
     var photoDir = dataDir + "images/";
     var childrensBooksDataURL = dataDir + "childrensbooks.json";
     var graphicNovelDataURL = dataDir + "graphicnovels.json";
     var merchandiseDataURL = dataDir + "merchandise.json";
-    // Get Graphic Novel Data
+    
+    //start clean slate
+    var numFound = 0; 
+    var searchCompletes = 0;
+    evt.preventDefault();
+    $results.html( "" );
+    $submit.prop("value", "Searching...");
+    
+    // grab search params, format into array
+    var searchFor = $("input").val();
+    var enteredTemp = searchFor.toLowerCase();
+    var commaGl = /,/gi;
+    var doubleSpace = /  /gi;
+    enteredTemp = enteredTemp.replace(commaGl, " ");
+    enteredTemp = enteredTemp.replace(doubleSpace, " ");
+    var searchTerms = enteredTemp.split(" ");
+    
+    // grab catagory selection and format
+    var searchInCat = $("select").val();
+    var displayCat;
+    if (!searchInCat) {
+        searchInCat = "";
+    }
+    if (!searchFor) {
+        searchFor = "";
+    } 
+    console.log("Submit Pressed with Params: " + searchTerms + " & " + searchInCat);
     
     // Build Found / No Matches Responses
-    var displayNotFound = '<h1 id="NumOfMatches"></h1>';
-    displayResults += "<p>";
-    
+    var displayResults = '<h1 id="NumOfMatches"></h1>';
+    displayResults += '<p>You searched for <b>';
     if (searchFor != "" && searchFor != "all") {
-        ifFor = ' for <b>' + searchFor + '</b> ';
+        for (var i=0; i<searchTerms.length; i++) {
+            displayResults += searchTerms[i] + ' ';
+        }
     } else {
-        ifFor = " for <b>Anything</b>";
+        displayResults += 'anything';
     }
+    displayResults += '</b> in <b>';
     if (searchInCat != "") {
         if (searchInCat === "childrensbooks") {
             displayCat = "Children\'s Books";
@@ -51,29 +62,14 @@ function searchProducts(evt) {
         } else if ( searchInCat === "all" ) {
             displayCat = "All Catagories";
         }
-        ifCat = ' in <b>' + displayCat + '</b>';
+        displayResults += displayCat + '</b>';
     } else {
-        ifCat = ' in <b>Any</b> Catagory';
+        displayResults += 'any</b> catagory';
     }
-    startResponce = 'You Searched';
-    displayNotFound += startResponce + ifFor + ifCat;
-    displayNotFound += '<br><br></p>';
-    $results.prepend(displayNotFound);
+    displayResults += '<br><br></p>';
+    console.log(displayResults);
+    $results.prepend(displayResults);
     
-    //start timer?
-    /*
-    function checkSearchProgress() {
-        if ( searchCompletes > 0) {
-            $submit.prop("value", "Submit");
-            $("#numOfMatches").text( numFound + " Matches" );
-            clearTimeout( progTimer );
-            alert("Finished searching...");
-        } else {
-            alert("Still searching...");
-        }
-    }
-    var progTimer = setTimeout( function () { checkSearchProgress() }, 200);
-    */
     if ( searchInCat === "graphicnovels" || searchInCat === "" || searchInCat === "all") {
         $.getJSON( graphicNovelDataURL, function ( response ) {
             var displayHTML = '<ul class="merchList" id="comics">';
@@ -81,33 +77,35 @@ function searchProducts(evt) {
             $.each( response, function ( index, value ) {
                 stringSearch = value.name + value.author + value.illustrator + value.series + value.description;
                 stringSearch = stringSearch.toLowerCase();
-                if ( searchFor === "" || stringSearch.indexOf( searchFor ) !== -1 ) {
-                    numFound++;
-                    displayHTML += '<li>';
-                        if ( value.coverPhoto != "" ) {
-                            displayHTML += '<img alt="' + value.series + ': ' + value.name + ', part ' + value.partNum + ' Cover Image" src="' + photoDir + value.coverPhoto + '">';
-                        } else {
-                            displayHTML += '<img alt="Cover Photo Unavailable" src="' + photoDir + 'h-np.jpg">';
-                        }
-                        displayHTML += '<button';
-                        if ( value.avail && value.price ) {
-                            displayHTML += ' class="avail"';
-                            console.log("issue available!");
-                        } else {
-                            console.log("issue unavailable :-(");
-                        }
-                        displayHTML += '>';
-                        if ( value.price ) { 
-                             displayHTML += '$' + value.price;
-                        } else {
-                            displayHTML += 'unavailable';
-                        }
-                        displayHTML += ' </button>';
-                        displayHTML += '<h1>' + value.name + '</h1><h3><i>(part ' + value.partNum + ' of ' + value.ofPart + ')</i></h3>';
-                        displayHTML += '<h3>' + value.series + ' - Issue: ' + value.issue + '</h3>';
-                        displayHTML += '<p>' + value.description + '</p>';
-                    displayHTML += '</li>';
-                }
+                for (var i=0; i<searchTerms.length; i++) {
+                    if ( searchFor === "" || stringSearch.indexOf( searchTerms[i] ) !== -1 ) {
+                        numFound++;
+                        displayHTML += '<li>';
+                            if ( value.coverPhoto !== "" ) {
+                                displayHTML += '<img alt="' + value.series + ': ' + value.name + ', part ' + value.partNum + ' Cover Image" src="' + photoDir + value.coverPhoto + '">';
+                            } else {
+                                displayHTML += '<img alt="Cover Photo Unavailable" src="' + photoDir + 'h-np.jpg">';
+                            }
+                            displayHTML += '<button';
+                            if ( value.avail && value.price ) {
+                                displayHTML += ' class="avail"';
+                                console.log("issue available!");
+                            } else {
+                                console.log("issue unavailable :-(");
+                            }
+                            displayHTML += '>';
+                            if ( value.price ) { 
+                                 displayHTML += '$' + value.price;
+                            } else {
+                                displayHTML += 'unavailable';
+                            }
+                            displayHTML += ' </button>';
+                            displayHTML += '<h1>' + value.name + '</h1><h3><i>(part ' + value.partNum + ' of ' + value.ofPart + ')</i></h3>';
+                            displayHTML += '<h3>' + value.series + ' - Issue: ' + value.issue + '</h3>';
+                            displayHTML += '<p>' + value.description + '</p>';
+                        displayHTML += '</li>';
+                    } //end if
+                } // end for
             }); // end each
             displayHTML += '</ul>';
             $results.append( displayHTML );
@@ -116,7 +114,11 @@ function searchProducts(evt) {
             }); // end click()
             searchCompletes++;
             $submit.prop("value", "Submit");
-            $("#NumOfMatches").text( numFound + " Matches" );
+            if (numFound !== 1) {
+                $("#NumOfMatches").text( numFound + " Matches" );
+            } else {
+                $("#NumOfMatches").text( numFound + " Match" );
+            }
         }); // end getJSON
     } //end If GN
     
@@ -128,33 +130,35 @@ function searchProducts(evt) {
             $.each( response, function ( index, value ) {
                 stringSearch = value.title + value.author + value.illustrator + value.description;
                 stringSearch = stringSearch.toLowerCase();
-                if ( searchFor === "" || stringSearch.indexOf( searchFor ) !== -1 ) {
-                    numFound++;
-                    displayHTML += '<li>';
-                        if ( value.coverPhoto != "" ) {
-                            displayHTML += '<img alt="' + value.title  + ' Cover Image" src="' + photoDir + value.coverPhoto + '">';
-                        } else {
-                            displayHTML += '<img alt="Cover Photo Unavailable" src="' + photoDir + 'ch-np.jpg">';
-                        }
-                        displayHTML += '<button';
-                        if ( value.avail && value.price ) {
-                            displayHTML += ' class="avail"';
-                            console.log("issue available!");
-                        } else {
-                            console.log("issue unavailable :-(");
-                        }
-                        displayHTML += '>';
-                        if ( value.price ) { 
-                             displayHTML += '$' + value.price;
-                        } else {
-                            displayHTML += 'unavailable';
-                        }
-                        displayHTML += ' </button>';
-                        displayHTML += '<h1>' + value.title + '</h1>';
-                        displayHTML += '<h3>Author: ' + value.author + '<br>Illustrations: ' + value.illustrator + '</h3>';
-                        displayHTML += '<p>' + value.description + '</p>';
-                    displayHTML += '</li>';
-                }
+                for (var i=0; i<searchTerms.length; i++) {
+                    if ( searchFor === "" || stringSearch.indexOf( searchTerms[i] ) !== -1 ) {
+                        numFound++;
+                        displayHTML += '<li>';
+                            if ( value.coverPhoto != "" ) {
+                                displayHTML += '<img alt="' + value.title  + ' Cover Image" src="' + photoDir + value.coverPhoto + '">';
+                            } else {
+                                displayHTML += '<img alt="Cover Photo Unavailable" src="' + photoDir + 'ch-np.jpg">';
+                            }
+                            displayHTML += '<button';
+                            if ( value.avail && value.price ) {
+                                displayHTML += ' class="avail"';
+                                console.log("issue available!");
+                            } else {
+                                console.log("issue unavailable :-(");
+                            }
+                            displayHTML += '>';
+                            if ( value.price ) { 
+                                 displayHTML += '$' + value.price;
+                            } else {
+                                displayHTML += 'unavailable';
+                            }
+                            displayHTML += ' </button>';
+                            displayHTML += '<h1>' + value.title + '</h1>';
+                            displayHTML += '<h3>Author: ' + value.author + '<br>Illustrations: ' + value.illustrator + '</h3>';
+                            displayHTML += '<p>' + value.description + '</p>';
+                        displayHTML += '</li>';
+                    } //end if
+                }//end for
             }); // end each
             displayHTML += '</ul>';
             $results.append( displayHTML );
@@ -163,7 +167,11 @@ function searchProducts(evt) {
             }); // end click()
             searchCompletes++;
             $submit.prop("value", "Submit");
-            $("#NumOfMatches").text( numFound + " Matches" );
+            if (numFound !== 1) {
+                $("#NumOfMatches").text( numFound + " Matches" );
+            } else {
+                $("#NumOfMatches").text( numFound + " Match" );
+            }
         }); // end getJSON
     } 
     // Get Merchandise Data
@@ -171,13 +179,40 @@ function searchProducts(evt) {
         $.getJSON( merchandiseDataURL, function ( response ) {
             var displayHTML = '<ul class="merchList" id="merch">';
             var stringSearch;
-            $.each( response, function ( index, value ) {
-                //stringSearch = value.name + value.author + value.illustrator + value.series + value.description;
+           $.each( response, function ( index, value ) {
+                stringSearch = value.name + value.type + value.description;
                 stringSearch = stringSearch.toLowerCase();
-                if ( searchFor === "" || stringSearch.indexOf( searchFor ) !== -1 ) {
-                    numFound++;
-                }
+                for (var i=0; i<searchTerms.length; i++) {
+                    if ( searchFor === "" || stringSearch.indexOf( searchTerms[i] ) !== -1 ) {
+                        numFound++;
+                        displayHTML += '<li>';
+                            if ( value.coverPhoto != "" ) {
+                                displayHTML += '<img alt="' + value.name  + ' Cover Image" src="' + photoDir + value.coverPhoto + '">';
+                            } else {
+                                displayHTML += '<img alt="Cover Photo Unavailable" src="' + photoDir + 'ch-np.jpg">';
+                            }
+                            displayHTML += '<button';
+                            if ( value.avail && value.price ) {
+                                displayHTML += ' class="avail"';
+                                console.log("issue available!");
+                            } else {
+                                console.log("issue unavailable :-(");
+                            }
+                            displayHTML += '>';
+                            if ( value.price ) { 
+                                 displayHTML += '$' + value.price;
+                            } else {
+                                displayHTML += 'unavailable';
+                            }
+                            displayHTML += ' </button>';
+                            displayHTML += '<h1>' + value.name + '</h1>';
+                            displayHTML += '<h3>Product: ' + value.type + '</h3>';
+                            displayHTML += '<p>' + value.description + '</p>';
+                        displayHTML += '</li>';
+                    } //end if
+                }//end for
             }); // end each
+
             displayHTML += '</ul>';
             $results.append( displayHTML );
             $("#merch .avail").click( function () {
@@ -185,7 +220,11 @@ function searchProducts(evt) {
             }); // end click()
             searchCompletes++;
             $submit.prop("value", "Submit");
-            $("#NumOfMatches").text( numFound + " Matches" );
+            if (numFound !== 1) {
+                $("#NumOfMatches").text( numFound + " Matches" );
+            } else {
+                $("#NumOfMatches").text( numFound + " Match" );
+            }
         }); // end getJSON
     } 
     
