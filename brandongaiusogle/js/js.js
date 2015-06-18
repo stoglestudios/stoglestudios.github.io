78ffe1e15817dbc808db73f5ed9f271f0ea0f7ea
Brandon Ogle <brandon@angelvisiontech.com>
2015-06-18 12:07:18 -0700

    test5

--- brandongaiusogle/js/js.js
+++ brandongaiusogle/js/js.js
@@ -77,7 +77,7 @@ $(document).ready(function() {
     $(".jumpup").click(function(evt) {
         evt.preventDefault();
         $('html, body').animate({
-            scrollTop: $(this).parents(".section").prev().offset().top
+            scrollTop: 0//$(this).parents("html").offset().top
         }, 2000);
     });
     $(window).on("mousewheel", function(event) {
