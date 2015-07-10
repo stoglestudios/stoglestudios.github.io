1d56d00d285cce85f06ab5a09f297dc5408f56ad
stoglestudios <brandongaius@gmail.com>
2015-07-10 16:53:20 -0700

    new portfolio page

--- portfolio/js/js.js
+++ portfolio/js/js.js
@@ -1,62 +1,18 @@
-var times = [3,51,132,189,216];
+
 
 $(document).ready(function() {
     console.log("Ready");
-    
-    var iframe = $('#movie')[0];
-    var player = $f(iframe);
-    var status = $('.status');
-    
-    player.addEvent("ready", videoReady);
-    
-    function videoReady() {
-        console.log("Vimeo Ready");
-        $("#char-chp").on("click", function() {
-            console.log("chapter 1");
-            player.api("play");
-            player.api("seekTo", times[0]);
-        });
-        $("#ed-chp").on("click", function() {
-            console.log("chapter 2");
-            player.api("play");
-            player.api("seekTo", times[1]);
-        });
-        $("#info-chp").on("click", function() {
-            console.log("chapter 3");
-            player.api("play");
-            player.api("seekTo", times[2]);
-        });
-        $("#stil-chp").on("click", function() {
-            console.log("chapter 4");
-            player.api("play");
-            player.api("seekTo", times[3]);
-        });
-        player.addEvent("finish ", function() {
-            $("#char-chp").removeClass("current");
-            $("#char-chp").siblings().removeClass("current");
-        });
-        player.addEvent("playProgress", function(data) {
-            var timeNow = data.seconds;
-            if (timeNow > times[0] && timeNow < times[1]) {
-                //console.log("Chapter 1 Started");
-                $("#char-chp").addClass("current");
-                $("#char-chp").siblings().removeClass("current");
-            } else if (timeNow > times[1] && timeNow < times[2]) {
-                //console.log("Chapter 1 Started");
-                $("#ed-chp").addClass("current");
-                $("#ed-chp").siblings().removeClass("current");
-            } else if (timeNow > times[2] && timeNow < times[3]) {
-                //console.log("Chapter 1 Started");
-                $("#info-chp").addClass("current");
-                $("#info-chp").siblings().removeClass("current");
-            } else if (timeNow > times[3] && timeNow < times[4]) {
-                //console.log("Chapter 1 Started");
-                $("#stil-chp").addClass("current");
-                $("#stil-chp").siblings().removeClass("current");
-            } else {
-                $("#char-chp").removeClass("current");
-                $("#char-chp").siblings().removeClass("current");
-            }
-        });
-    };
+    $(".unsubscribe").hide();
+    $(".more").show();
+    $(".more").on("click", function(event) {
+        $(".unsubscribe").show();
+        $(".more").hide();
+        $(".less").show();
+    });
+    $(".less").on("click", function(event) {
+        $(".unsubscribe").hide();
+        $(".less").hide();
+        $(".more").show();
+    });
 });
+ 
