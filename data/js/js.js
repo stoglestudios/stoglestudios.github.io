e1723a2049ca7eaeb83361e7a7b56a768b7cb7cd
brandongaius <brandongaius@gmail.com>
2015-10-08 10:07:40 -0700

    att to body

--- data/js/js.js
+++ data/js/js.js
@@ -66,7 +66,7 @@ $(document).ready(function() {
                 }
             }
             styleTag += "</style>";
-            $("head").append(styleTag);
+            $("body").append(styleTag);
         }
     });
     repeatText( "#repeat" );
