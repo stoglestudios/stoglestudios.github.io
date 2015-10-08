cfa9b75ba140e7be6ef41a0d5eed65d35548f7a5
brandongaius <brandongaius@gmail.com>
2015-10-08 14:42:25 -0700

    test ie 8

--- data/js/js.js
+++ data/js/js.js
@@ -47,7 +47,7 @@ $(document).ready(function() {
 });
 
 function repeatText( theTarget ) {
-    var repeatCycleNumber = Number($(theTarget).data("ektron-repeat"));
+    var repeatCycleNumber = Number( $(theTarget).data("ektron-repeat") );
     var theStartText = $(theTarget).text();
     var theText = theStartText;
     for (var i=0; i<repeatCycleNumber; i++) {
