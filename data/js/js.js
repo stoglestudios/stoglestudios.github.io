96f29608f4b3d730a5a491b283a05024337bf3ee
brandongaius <brandongaius@gmail.com>
2015-10-08 15:19:53 -0700

    ie <9 7

--- data/js/js.js
+++ data/js/js.js
@@ -45,17 +45,17 @@ $(document).ready(function() {
     });
 
     //Example Function
-    repeatText( "#repeat" );
+    repeatText( $("#repeat") );
 });
 
 function repeatText( theTarget ) {
-    var repeatCycleNumber = Number( $(theTarget).data("ektron-repeat") );
-    var theStartText = $(theTarget).text();
+    var repeatCycleNumber = Number( theTarget.data("ektron-repeat") );
+    var theStartText = theTarget.text();
     var theText = theStartText;
     for (var i=0; i<repeatCycleNumber; i++) {
         theText += theStartText;
     }
-    $(theTarget).text(theText);
+    theTarget.text(theText);
     alert("function ran");
 }
 
