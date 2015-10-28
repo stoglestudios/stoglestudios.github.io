89b6585f2fa53403e9738df0ef47bd6da3f00fe9
brandongaius <brandongaius@gmail.com>
2015-10-28 12:07:04 -0700

    dvds

--- ham/js/menu.js
+++ ham/js/menu.js
@@ -78,6 +78,7 @@ function buildFloatingMenu () {
     //Get HTML of Menu & Assign it new id: "floating-menu"
     var menuMarkup = $( "<" + $("#menu").prop("tagName") + " />" ).append( $( "#menu" ).clone() ).html()/*.replace('id="menu"', 'id="floating-menu"')*/;
     menuMarkup = menuMarkup.replace('id="menu"', 'id="floating-menu"');
+    menuMarkup = menuMarkup.replace("id='menu'", "id='floating-menu'");
     //Make copy of Permanant Top Menu and add it to the body
     $("body").append(menuMarkup);
     //give it new styles:
