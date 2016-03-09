3ca169cff451526669d4c977303fd6cbad591d92
Brandon Ogle <brandon@angelvisiontech.com>
2016-03-09 13:01:16 -0800

    add close and prev btns

--- heretics/js/js.js
+++ heretics/js/js.js
@@ -17,6 +17,7 @@ var $openers = [
 var $intro = $('#intro');
 var $skip = $(".skip");
 var $tabs = $("nav li a");
+var lastDivToLoad = [];
 
 // Functions
 $skip.click( function() {
@@ -27,7 +28,7 @@ $skip.click( function() {
     });
     $('#page').css({opacity: "0"}).show().delay(200).animate({opacity: "1"}, 200);
     $(this).hide();
-    console.log("Skip Button Pressed");
+    //console.log("Skip Button Pressed");
 });
 // Function to animate opening sequence - REWRITE
 (function( $ ){
@@ -37,7 +38,7 @@ $skip.click( function() {
         $thisImg = $this.children("img");
         var marH = parseInt($thisImg.css("margin-top"));
         marH = Math.round( 100*marH/$(window).height() );
-        console.log(marH);
+        //console.log(marH);
         var durationOS = 2000;
         var fadeTime = 2000;
         var totaltime = durationOS + fadeTime*2;
@@ -100,7 +101,7 @@ $(document).ready(function(){
         if ( !$this.parent().hasClass("selected") ) {
             var pageClicked = $this.html().toLowerCase();
             pageClicked = "#" + pageClicked;
-            console.log(pageClicked);
+            //console.log(pageClicked);
             $this.parent().toggleClass("selected");
             $this.parent().siblings().removeClass("selected");
             $("content").hide();
@@ -113,15 +114,27 @@ $(document).ready(function(){
         var getAttrHREF = event.target.getAttribute("href");
         var getAttrClass = event.target.getAttribute("class");
         var getAttrTag = event.target.tagName.toLowerCase();
-        if (getAttrID == "modal" || getAttrClass == "backbutton") {
+        if (getAttrID == "modal" || getAttrClass == "closebutton") {
             $("#modal").animate({opacity: "0"}, 200, function(){
                 $this = $(this);
                 $this.hide();
                 $this.children("div").remove();
             });
+        } else if (getAttrClass == "prevbutton" && lastDivToLoad.length>0) {
+            $("#modalBox").attr("id", "oldBox").show();
+            $("#oldBox").animate({opacity: "0", marginLeft: "100%"}, 200, "easeInOutCirc", function(){
+                $(this).remove();
+            });
+            console.log(lastDivToLoad[lastDivToLoad.length-1]);
+            $(lastDivToLoad[lastDivToLoad.length-2]).clone().appendTo("#modal").attr("id", "modalBox").css({opacity: "0", marginLeft: "-100%"}).show().animate({opacity: "1", marginLeft: "0%"}, 200, "easeInOutCirc");
+            if (lastDivToLoad.length>2) {
+                $("#modalBox .prevbutton").css("display", "inline-block");
+            } else {
+                $("#modalBox .prevbutton").css("display", "none");
+            }
+            lastDivToLoad.pop();
         } else {
-            console.log("Is: " + getAttrTag + ", href: " + getAttrHREF);
-            if (getAttrTag == "a" && getAttrClass != "backbutton") {
+            if (getAttrTag == "a" && getAttrClass != "closebutton"  && getAttrClass != "prevbutton") {
                 var divToLoad = getAttrHREF;
                 if ($("#modalBox") && $("#modalBox").length>0) {
                     $("#modalBox").attr("id", "oldBox");
@@ -129,9 +142,17 @@ $(document).ready(function(){
                         $(this).remove();
                     });
                     $(divToLoad).clone().appendTo("#modal").attr("id", "modalBox").css({opacity: "0", marginLeft: "100%"}).show().delay(100).animate({opacity: "1", marginLeft: "0%"}, 200, "easeInOutCirc");
+                    $("#modalBox .prevbutton").css("display", "inline-block");
                 } else {
                     $(divToLoad).clone().appendTo("#modal").attr("id", "modalBox").css({opacity: "0"}).show().animate({opacity: "1"}, 200, "easeInOutCirc");
+                    
                 }
+                if (lastDivToLoad.length>0) {
+                    $("#modalBox .prevbutton").css("display", "inline-block");
+                } else {
+                    $("#modalBox .prevbutton").css("display", "none");
+                }
+                lastDivToLoad.push(divToLoad);
             }
         }
     });
@@ -141,8 +162,7 @@ $(document).ready(function(){
         $this = $(this);
         var divToLoad = $this.attr("href");
         var parentID = $this.parents("div").attr("id");
-        console.log(".inpagelink button pressed - Link: " + divToLoad + " - Parent: " + parentID );
-        //
+        //console.log(".inpagelink button pressed - Link: " + divToLoad + " - Parent: " + parentID );
         $("#modal").show().animate({opacity: "1"}, 200);
         
         if ($("#modalBox") && $("#modalBox").length>0) {
@@ -153,6 +173,13 @@ $(document).ready(function(){
             $(divToLoad).clone().appendTo("#modal").attr("id", "modalBox").css({opacity: "0", marginLeft: "100%"}).show().animate({opacity: "1", marginLeft: "0%"}, 200, "easeInOutCirc");
         } else {
             $(divToLoad).clone().appendTo("#modal").attr("id", "modalBox").css({opacity: "0"}).show().animate({opacity: "1"}, 200, "easeInOutCirc");
+            $("#modalBox .prevbutton").css("display", "none");
         }
+        if (lastDivToLoad.length>0) {
+            $("#modalBox .prevbutton").css("display", "inline-block");
+        } else {
+            $("#modalBox .prevbutton").css("display", "none");
+        }
+        lastDivToLoad.push(divToLoad);
     });
 });
