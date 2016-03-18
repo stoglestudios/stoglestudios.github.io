a437c32684af202fac11cf0873b8f9a728432b55
Brandon Ogle <brandon@angelvisiontech.com>
2016-03-18 13:36:55 -0700

    stuff

--- heretics/js/js.js
+++ heretics/js/js.js
@@ -80,15 +80,15 @@ $skip.click( function() {
 $(document).ready(function(){
     var cardNav = "<a class='prevbutton'>Prev</a><a class='closebutton'>Close</a>";
     $(".characters, .definitions").prepend(cardNav);
-    //Build assets
+    // Build assets
     
     $("#character-app").show();
     $(".characters").hide();
-    
+    $("#sample-menu").show();
     $("#cosmological-map").show();
     $(".definitions").hide();
     
-    //Hide/show appropriate sections
+    // Hide/show appropriate sections
     $('#characters, #cosmology, #samples, #author, #contact').hide();
     
     //Initiate animated opener sections -> REWRITE
@@ -101,7 +101,7 @@ $(document).ready(function(){
             backgroundPositionY: '100%',
         }, 24000, function() {
             $('#intro').fadeOut(2000);
-            //setCookie("seenit", "true", 1);
+            // setCookie("seenit", "true", 1);
         });
         for (var i=0;i<$openers.length;i++) {
             var j = Math.floor(i/($openers.length-1)) ? true : false;
@@ -112,16 +112,25 @@ $(document).ready(function(){
             var getURL = "#" + window.location.href.split("#")[1];
             $("content").hide();
             $(getURL).show();
+            //$('#page').scrollTop(0);
         }
+        $("nav ul li").each(function(){
+            $this = $(this);
+            if ( $this.children("a").attr("href") == getURL ) {
+                $this.addClass("selected");
+            } else {
+                $this.removeClass("selected");
+            }
+        });
     }
     //Toggle Sections
     $tabs.click(function(event) {
         $this = $(this);
-        event.preventDefault();
+        //event.preventDefault();
         if ( !$this.parent().hasClass("selected") ) {
             var pageClicked = $this.html().toLowerCase();
             pageClicked = "#" + pageClicked;
-            //console.log(pageClicked);
+            // console.log(pageClicked);
             $this.parent().toggleClass("selected");
             $this.parent().siblings().removeClass("selected");
             $("content").hide();
@@ -169,13 +178,12 @@ $(document).ready(function(){
                     $("#modalBox .prevbutton").css("display", "none");
                 }
             });
-            
 // ************************************  LINK TO NEXT CARD  ************************************
         } else {
             if (getAttrTag == "a" && getAttrClass != "closebutton"  && getAttrClass != "prevbutton") {
                 var divToLoad = getAttrHREF;
                 if ($("#modalBox") && $("#modalBox").length>0) {
-                    // ************************************  OLD CARD  ************************************q
+                    // ************************************  OLD CARD  ************************************
                     $("#modalBox").show().animate({
                         opacity: "0", 
                         marginLeft: "-100%"
@@ -267,6 +275,53 @@ $(document).ready(function(){
             boxShadow: "0 0 10px black"
         });
     });
+    $(".sample-carousel").each(function(){
+        if(typeof carNum !== 'undefined'){
+            carNum++;
+        } else {
+            carNum=1;
+        }
+        console.log(carNum);
+        var $this = $(this);
+        $this.addClass("carousel-"+carNum);
+        var numOfSlides = $this.children("div").length;
+        console.log("Slides: " + numOfSlides);
+        var navBuilderString = "<nav class='carousel-controls'>";
+        navBuilderString += "<div class='carousel-back car-btn'></div><ul>";
+        for (i=1;i<=numOfSlides;i++) {
+            navBuilderString += "<li class='car-slide-btn car-btn' data-target-slide='.carousel-slide" + i + "'></li>";
+            $this.children(".carousel-slide:nth-child(" + i + ")").addClass("carousel-slide"+i);
+        }
+        navBuilderString += "</ul><div class='carousel-playpause car-btn'>";
+        navBuilderString += "<div class='carousel-pause'></div>";
+        navBuilderString += "<div class='carousel-play'></div></div>";
+        navBuilderString += "<div class='carousel-forward car-btn'></div></nav>";
+        $this.prepend(navBuilderString);
+        $(".carousel-" + carNum + " .carousel-playpause>div").on("click", function(){
+            $(this).siblings().show();
+            $(this).hide();
+        })
+        $(".carousel-" + carNum + " .car-slide-btn").on("click", function() {
+            $t = $(this);
+            advanceSlideShow( $t.data("target-slide") );
+        });
+        $(".carousel-" + carNum + " .carousel-back").on("click", function() {
+            scrubSlideShow ( false, $(this), true );
+        });
+        $(".carousel-" + carNum + " .carousel-forward").on("click", function() {
+            scrubSlideShow ( true, $(this), true );
+        });
+    });
+    var slideShowID = window.setInterval(function(){
+        $(".news-carousel").each(function(){
+            $this = $(this);
+            $playpause = $this.children("nav").children(".carousel-playpause");
+            $pause = $playpause.children(".carousel-pause");
+            if ( $pause.is(":visible") ) {
+                scrubSlideShow ( true, $playpause, false );
+            }
+        });
+    }, 5000);
 });
 
 function setCookie(cname, cvalue, exdays) {
@@ -284,4 +339,83 @@ function getCookie(cname) {
         if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
     }
     return "";
+}
+var slideTime = 200;
+function advanceSlideShow ( target ) {
+    var $targetSlide = $( target );
+    var $parent = $targetSlide.parent();
+    $parent.children("nav").children(".carousel-playpause").children(".carousel-pause").hide();
+    $parent.children("nav").children(".carousel-playpause").children(".carousel-play").show();
+    var $currentSlide;
+    $targetSlide.siblings("div").each( function() {
+        if ( $(this).css("left") == "0px" ) {
+            $currentSlide = $( this );
+        }
+    });
+    var goForward = true;
+    if ( $parent.children("div").index( $targetSlide ) < $parent.children("div").index( $currentSlide ) ) {
+        goForward = false;
+    }
+    if ( goForward ) {
+        $targetSlide.css("left", "100%").animate({
+            left: "0%"
+        }, slideTime);
+        $currentSlide.animate({
+            left: "-100%"
+        }, slideTime);
+    } else {
+        $targetSlide.css("left", "-100%").animate({
+            left: "0%"
+        }, slideTime);
+        $currentSlide.animate({
+            left: "100%"
+        }, slideTime);
+    }
+}
+function scrubSlideShow ( forward, obj, press ) {
+    var $button = obj;
+    var $parent = obj.parent().parent();
+    var $currentSlide = $parent.children(".carousel-slide1");
+    $parent.children("div").each( function() {
+        if ( $(this).css("left") == "0px" ) {
+            $currentSlide = $( this );     
+        }
+    });
+    var childNumber = $parent.children("div").index( $currentSlide ) + 1;
+    var totalNumber = $parent.children("div").length;
+    var $next;
+    var $prev;
+    var prevChildNum;
+    var nextChildNum;
+    if ( childNumber > 1 ) {
+        prevChildNum = childNumber -1;
+    } else {
+        prevChildNum = totalNumber;
+    }
+    if ( childNumber < totalNumber ) {
+        nextChildNum = childNumber + 1;
+    } else {
+        nextChildNum = 1;
+    }
+    if ( forward ) {
+        $currentSlide.css("left", "0%").animate({
+            left: "-100%"
+        }, slideTime);
+        $next = $currentSlide.siblings(".carousel-slide" + nextChildNum );
+        $next.css("left", "100%").animate({
+            left: "0%"
+        }, slideTime);
+    } else {
+        $currentSlide.css("left", "0%").animate({
+            left: "100%"
+        }, slideTime);
+        $prev = $currentSlide.siblings(".carousel-slide" + prevChildNum );
+        $prev.css("left", "-100%").animate({
+            left: "0%"
+        }, slideTime);
+    }
+    if ( press ) {
+        $parent.children("nav").children(".carousel-playpause").children(".carousel-pause").hide();
+        $parent.children("nav").children(".carousel-playpause").children(".carousel-play").show();
+    }
 }
